6e3fa4ceacf57f5413fcdf9063c1811b3ded3539
btnvlscoder <btnvlscoder@gmail.com>
2025-10-22 03:35:43 -0300

    Se agrega estructura de trabajo en region; repository, controller, service... pruebas en postman ok, get-post-postlista

---
+++ src/main/java/bcorona_ftoloza/backendMySql_levelUpGamer/controller/RegionController.java
@@ -0,0 +1,38 @@
+package bcorona_ftoloza.backendMySql_levelUpGamer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import bcorona_ftoloza.backendMySql_levelUpGamer.model.Region;
+import bcorona_ftoloza.backendMySql_levelUpGamer.service.RegionService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/regiones")
+public class RegionController {
+    @Autowired
+    private RegionService regionService;
+
+    @GetMapping("/listar")
+    public List<Region> listarRegiones() {
+        return regionService.listar();
+    }
+    
+    @PostMapping("/agregarRegion")
+    public void agregarRegion(@RequestBody Region region) {
+        regionService.agregarRegion(region);
+    }
+    @PostMapping("/agregarListaRegion")
+    public void agregarRegion(@RequestBody List<Region> regiones) {
+        for (Region region : regiones) {
+            regionService.agregarRegion(region);
+        }
+    }
+
+}
---
+++ src/main/java/bcorona_ftoloza/backendMySql_levelUpGamer/service/RegionService.java
@@ -0,0 +1,30 @@
+package bcorona_ftoloza.backendMySql_levelUpGamer.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bcorona_ftoloza.backendMySql_levelUpGamer.model.Region;
+import bcorona_ftoloza.backendMySql_levelUpGamer.repository.RegionRepository;
+
+@Service
+public class RegionService {
+    @Autowired
+    private RegionRepository regionRepository;
+
+    public String agregarRegion(Region region) {
+        Region validacion = regionRepository.findByNombre(region.getNombre());
+        if(validacion == null) {
+            regionRepository.save(region);
+            return "Region almacenada correctamente!";
+        } else {
+            return "Region ya ingresada!";
+        }
+    }
+
+    public List<Region> listar() {
+        return regionRepository.findAll();
+    }
+
+}
