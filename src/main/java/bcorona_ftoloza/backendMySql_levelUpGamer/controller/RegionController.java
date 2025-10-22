aff12b747e4f5b30934e7291524ccfdb1307b6cb
btnvlscoder <btnvlscoder@gmail.com>
2025-10-22 03:49:10 -0300

    Se comenta codigo con intrucciones para avance

--- src/main/java/bcorona_ftoloza/backendMySql_levelUpGamer/controller/RegionController.java
+++ src/main/java/bcorona_ftoloza/backendMySql_levelUpGamer/controller/RegionController.java
@@ -12,27 +12,55 @@ import bcorona_ftoloza.backendMySql_levelUpGamer.model.Region;
 import bcorona_ftoloza.backendMySql_levelUpGamer.service.RegionService;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+//definimos el controlador con restcontroller
 @RestController
-@RequestMapping("/regiones")
+@RequestMapping("/regiones")//creamos la ruta principal del controlador "/regiones"
 public class RegionController {
+    //inyectamos el servicio del modelo del controlador
     @Autowired
     private RegionService regionService;
-
+    //getmapping para configurar la ruta listar, en donde se mostraran todas las regiones almacenadas
     @GetMapping("/listar")
     public List<Region> listarRegiones() {
         return regionService.listar();
     }
-    
+    //postmapping para configurar la ruta agregarRegion, en donde se podra agregar una nueva region
     @PostMapping("/agregarRegion")
     public void agregarRegion(@RequestBody Region region) {
         regionService.agregarRegion(region);
     }
+    //este postmapping es para agregar una lista de regiones, util para poblar la base de datos como las regiones de chile son fijas
     @PostMapping("/agregarListaRegion")
     public void agregarRegion(@RequestBody List<Region> regiones) {
         for (Region region : regiones) {
             regionService.agregarRegion(region);
         }
     }
+    /*despues de crear el controlador, se debe probar en postman
+    seleccionas el metodo get o post y pruebas las diferentes rutas, recuerda que es 
+    http://localhost:8080/regiones/listar o http://localhost:8080/regiones/agregarRegion
+    se agregan en formato json, puedes pasarle el modelo a una ia para que te genere datos de prueba
+    ejemplo de json para agregarRegion
+    
+    {
+        "nombre": "Region de prueba"
+    }
+    ejemplo de json para agregarListaRegion
+
+    [
+        {
+            "nombre": "Region 1"
+        },
+        {
+            "nombre": "Region 2"
+        },
+        {
+            "nombre": "Region 3"
+        }
+    ]
+
+
+    y vamos que se puede! nos toca duro pero aham asi es nuestro mundo buena suerte!
+    */
 
 }
